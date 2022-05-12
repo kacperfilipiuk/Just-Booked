70f48ae8cb508240dba2158adb13fba9b2df381a
Varodann <kacperfilipiuk@outlook.com>
2022-05-12 23:02:00 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.projekt_z_javy {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.projekt_z_javy to javafx.fxml;
+    exports com.example.projekt_z_javy;
+}
