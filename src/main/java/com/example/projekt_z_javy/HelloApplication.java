a24d323267ebf4237503cbfa10d1cf65bbfa4f74
Varodann <kacperfilipiuk@outlook.com>
2022-05-13 14:41:54 +0200

    v 0.2 Zmiany interfejsu

--- src/main/java/com/example/projekt_z_javy/HelloApplication.java
+++ src/main/java/com/example/projekt_z_javy/HelloApplication.java
@@ -2,22 +2,32 @@ package com.example.projekt_z_javy;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Stage stg;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root  = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("Falcon");
+        primaryStage.setScene( new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
